3530bc40c0789e17ea3214e258fb99df546ecde7
samlovelace <samlovelace12@gmail.com>
2024-09-09 19:11:33 -0400

    updates Apple and Segment to use common base class

---
+++ include/Entity.h
@@ -0,0 +1,27 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "SFML/Graphics.hpp"
+#include "constants.h"
+
+class Entity
+{
+
+public:
+    Entity(int width, int height) : mDrawable(sf::RectangleShape(sf::Vector2f(width, height))) {}
+    virtual ~Entity() {}
+
+    // ******* Getters/Setters ********** // 
+    void setPosition(int x, int y) {mDrawable.setPosition(x,y); }
+    void setColor(sf::Color aColor) {mDrawable.setFillColor(aColor); }
+    int height() {return mDrawable.getSize().y; }
+    int width() {return mDrawable.getSize().x; }
+    float x() {return mDrawable.getPosition().x; }
+    float y() {return mDrawable.getPosition().y; }
+    sf::RectangleShape getDrawable() {return mDrawable;}
+
+private:
+    sf::RectangleShape mDrawable; 
+};
+
+#endif //ENTITY_H
